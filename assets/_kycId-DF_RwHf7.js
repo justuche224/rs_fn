21eab06dfc52ffe3eaa9834d55c5e5ee41d8610c
justuche224 <justuche224@gmail.com>
2025-04-12 23:32:08 +0100

    first commit

---
+++ assets/_kycId-DF_RwHf7.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BJX2Zmj2.js";const e=function(){return o.jsx("div",{children:'Hello "/admin/kyc/$kycId"!'})};export{e as component};
---
+++ assets/index-IqZa-8qe.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BJX2Zmj2.js";const n=()=>o.jsx("div",{children:"hello"});export{n as component};
