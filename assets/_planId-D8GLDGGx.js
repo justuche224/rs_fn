ce74f860597d93d37eab21be584af93e252e5b4f
justuche224 <justuche224@gmail.com>
2025-04-16 09:10:42 +0100

    adjust user balance

---
+++ assets/_planId-D8GLDGGx.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DN1k3KKq.js";const s=({error:r})=>{const o=r.message??"An error occurred";return e.jsx("div",{children:o})};export{s as errorComponent};
---
+++ assets/index-DHnmm4Cl.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DN1k3KKq.js";const n=()=>o.jsx("div",{children:"hello"});export{n as component};
