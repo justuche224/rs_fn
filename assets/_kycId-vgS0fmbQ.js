34e2e699a0fdd8704cab8acccd0bf66734b3e54a
justuche224 <justuche224@gmail.com>
2025-04-18 10:29:25 +0100

    debug

--- assets/_kycId-vgS0fmbQ.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-Z5TVvw63.js";const e=function(){return o.jsx("div",{children:'Hello "/admin/kyc/$kycId"!'})};export{e as component};
--- assets/index-ClbPz8EA.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-Z5TVvw63.js";const n=()=>o.jsx("div",{children:"hello"});export{n as component};
