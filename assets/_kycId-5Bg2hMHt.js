7359a51889c55ae3a0bcac2cec382f2b1f074918
justuche224 <justuche224@gmail.com>
2025-04-19 07:44:12 +0100

    use new api url

--- assets/_kycId-5Bg2hMHt.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-CT-BcIIy.js";const e=function(){return o.jsx("div",{children:'Hello "/admin/kyc/$kycId"!'})};export{e as component};
--- assets/index-CV4in_Lz.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-CT-BcIIy.js";const n=()=>o.jsx("div",{children:"hello"});export{n as component};
