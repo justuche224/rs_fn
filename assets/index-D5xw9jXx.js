66e9a41738a41301a74eb30a247bebcaad728c18
justuche224 <justuche224@gmail.com>
2025-12-26 20:29:54 +0100

    fixes

---
+++ assets/index-D5xw9jXx.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-azKcz7jo.js";const n=()=>o.jsx("div",{children:"hello"});export{n as component};
--- assets/index-DudKFqJ7.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-iDHF-Q5J.js";const n=()=>o.jsx("div",{children:"hello"});export{n as component};
